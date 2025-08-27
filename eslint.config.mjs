c7303611fc68273c2bfad73922a46592ae2860aa
Kseniia Zinchenko <kzinchenko@danavero.com>
2025-08-27 20:08:27 +0300

    feat: updated eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,7 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+import { FlatCompat } from '@eslint/eslintrc';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -10,16 +11,59 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.extends('next/core-web-vitals', 'next/typescript', 'plugin:import/recommended', 'plugin:import/typescript', 'prettier'),
   {
     ignores: [
-      "node_modules/**",
-      ".next/**",
-      "out/**",
-      "build/**",
-      "next-env.d.ts",
+      'node_modules/**',
+      '.next/**',
+      'out/**',
+      'build/**',
+      'next-env.d.ts',
     ],
   },
+  {
+    settings: {
+      'import/resolver': { typescript: true, node: true },
+      'import/internal-regex': '^@/',
+      react: { version: 'detect' },
+    },
+  },
+  {
+    rules: {
+      quotes: ['warn', 'single', { avoidEscape: true }],
+      'jsx-quotes': ['warn', 'prefer-single'],
+      'no-unused-expressions': 'warn',
+      'import/newline-after-import': 'warn',
+      'import/no-duplicates': 'warn',
+
+      'import/order': [
+        'warn',
+        {
+          groups: [
+            'builtin',
+            'external',
+            'internal',
+            'type',
+            ['parent', 'sibling', 'index'],
+            'object',
+          ],
+          pathGroups: [
+            {
+              pattern: 'react',
+              group: 'external',
+              position: 'before',
+            },
+          ],
+          pathGroupsExcludedImportTypes: ['builtin'],
+          'newlines-between': 'always',
+          alphabetize: { order: 'asc', caseInsensitive: true },
+        },
+      ],
+
+      '@typescript-eslint/consistent-type-imports': ['warn', { prefer: 'type-imports' }],
+      '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_', varsIgnorePattern: '^_' }],
+    },
+  }
 ];
 
 export default eslintConfig;
